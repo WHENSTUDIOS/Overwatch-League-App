9ba8872e0e50bb1fbef19dd6669d0e7ec3df33ec
whenofficial <fsmitchm@gmail.com>
2018-05-13 19:14:08 +0400

    do some init work

---
+++ assets/data/backgrounds.js
@@ -0,0 +1,3 @@
+module.exports.images = [
+    'https://cdn-images-1.medium.com/max/1600/1*HNS7JFbId9o3iWpYo5nr0w.jpeg'
+]
---
+++ controllers/dashboard.js
@@ -0,0 +1,16 @@
+const electron = require('electron')
+const app = require('electron').app
+const path = require('path')
+const ipc = electron.ipcRenderer
+const axios = require('axios')
+const remote = require('electron').remote
+const BrowserWindow = electron.remote.BrowserWindow
+
+
+
+var btnClose = document.getElementById('close');
+
+btnClose.addEventListener('click', function() {
+    var win = remote.getCurrentWindow();
+    win.close();
+});
